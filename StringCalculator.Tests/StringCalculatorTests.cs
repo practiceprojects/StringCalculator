07ee44ef139e3d84ea90b1da1bbcfe86ed3aeef3
malloryrbush <malloryrbush@hotmail.com>
2018-08-18 19:40:42 -0400

    first push

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("1, 2", 3)]
+        [TestCase("2, 2", 4)]
+        [TestCase("3, 3, 3", 9)]
+        [TestCase("//;\n1;2")]
+        public void Add_Returns_Expected_Value(string input, int expected)
+        {
+            var calc = new StringCalculator();
+
+            var result = calc.Add(input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
+
+
+
+
+
+//TESTS BEFORE REFACTORING
+
+//[Test]
+//public void Add_Returns_1_For_1()
+//{
+//    var calc = new StringCalculator();
+
+//    var result = calc.Add("1");
+
+//    Assert.That(result, Is.EqualTo(1));
+//}
+
+//[Test]
+//public void Add_Returns_2_For_2()
+//{
+//    var calc = new StringCalculator();
+
+//    var result = calc.Add("2");
+
+//    Assert.That(result, Is.EqualTo(2));
+//}
+
+//[Test]
+//public void Add_Returns_3_For_1Comma2()
+//{
+//    var calc = new StringCalculator();
+
+//    var result = calc.Add("1,2");
+
+//    Assert.That(result, Is.EqualTo(3));
+//}
+
+//[Test]
+//public void Add_Returns_4_For_2Comma2()
+//{
+//    var calc = new StringCalculator();
+
+//    var result = calc.Add("2,2");
+
+//    Assert.That(result, Is.EqualTo(4));
+//}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            if (input == string.Empty)
+                return 0;
+
+            if (input.Contains(","))
+            {
+                return CommaSeparatedValuesSum(input);
+            }
+            return int.Parse(input);
+            }
+
+        private static int CommaSeparatedValuesSum(string input)
+        {
+            var numberStrings = input.Split(',');
+
+            var total = 0;
+            foreach (var number in numberStrings)
+            {
+                total = total + int.Parse(number);
+            }
+            return total;
+        }
+    }
+    } 
